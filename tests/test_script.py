879c423627d5e5ca50cc87de38b0a4c2f8f16932
Lokesh <imlokeshak@gmail.com>
2024-02-05 17:32:28 +0530

    pre

--- tests/test_script.py
+++ tests/test_script.py
@@ -2,18 +2,18 @@ import unittest
 
 from main import app
 
-# class TestToPerform(unittest.TestCase):
-#     def setUp(self):
-#         self.app = app.test_client()
+class TestToPerform(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
 
-#     def tearDown(self):
-#         pass
+    def tearDown(self):
+        pass
 
-#     def test_page(self):
-#         response = self.app.get('/', follow_redirects=True)
-#         print(response)
-#         self.assertEqual(response.status_code, 200)
+    def test_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        print(response)
+        self.assertEqual(response.status_code, 200)
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
